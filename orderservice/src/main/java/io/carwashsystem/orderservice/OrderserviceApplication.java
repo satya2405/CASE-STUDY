c746a0b19f77d8491ff8fcfce9cbd22e19341903
unknown <121710409033@GITAM.IN>
2021-12-06 13:19:53 +0530

    FIRST COMMIT

---
+++ orderservice/src/main/java/io/carwashsystem/orderservice/OrderserviceApplication.java
@@ -0,0 +1,21 @@
+package io.carwashsystem.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import io.carwashsystem.orderservice.service.OrderServiceImpl;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class OrderserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderserviceApplication.class, args);
+	}
+	@Bean
+	public OrderServiceImpl getcustomerService(){
+		return new OrderServiceImpl();
+	}
+	
+}
